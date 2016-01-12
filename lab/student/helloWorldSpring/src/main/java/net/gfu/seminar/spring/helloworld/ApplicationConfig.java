147202b46c2a9a87bb4023dd82a6a6413b57ccb7
bluetonic <ipansmail@gmail.com>
2016-01-12 14:12:23 +0100

    Better approach using Java based Cofiguration

--- lab/student/helloWorldSpring/src/main/java/net/gfu/seminar/spring/helloworld/ApplicationConfig.java
+++ lab/student/helloWorldSpring/src/main/java/net/gfu/seminar/spring/helloworld/ApplicationConfig.java
@@ -1,23 +1,39 @@
 package net.gfu.seminar.spring.helloworld;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 /**
  * Created by im on 12.01.2016.
  */
 
 @Configuration
+@PropertySource(value = {"classpath:/guest.properties"})
 public class ApplicationConfig {
+
     @Bean
-    public Greeting greeting(){
-        return  new Greeting(specialGuest());
+    public static PropertySourcesPlaceholderConfigurer pspc(){
+        return new PropertySourcesPlaceholderConfigurer();
     }
 
+    @Bean(initMethod = "init", destroyMethod = "destroy")
+    public Greeting greeting(SpecialGuest specialGuest){
+        return  new Greeting(specialGuest);
+    }
+/*
     @Bean
     public SpecialGuest specialGuest() {
         return new SpecialGuest("Mr.","VIP");
+    }
+*/
+
+    @Bean @Scope("prototype")
+    public SpecialGuest specialGuest(@Value("${firstname}") String firstname, @Value("${lastname}") String lastname ) {
+        return new SpecialGuest(firstname,lastname);
     }
 
     @Bean
