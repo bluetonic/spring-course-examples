6266328b6bd4f9eb489b51eb5e3b70f6f5edbd5e
bluetonic <ipansmail@gmail.com>
2016-01-13 10:02:08 +0100

    hyperSQL configuration added using Java based configuration  write a test , check if  connection established

--- lab/student/helloWorldSpring/src/main/java/net/gfu/seminar/spring/helloworld/ApplicationConfig.java
+++ lab/student/helloWorldSpring/src/main/java/net/gfu/seminar/spring/helloworld/ApplicationConfig.java
@@ -1,5 +1,6 @@
 package net.gfu.seminar.spring.helloworld;
 
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -9,22 +10,25 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
 
+import javax.sql.DataSource;
+
 /**
  * Created by im on 12.01.2016.
  */
 
 @Configuration
-@PropertySource(value = {"classpath:/guest.properties"})
+@PropertySource(value = {"classpath:/guest.properties", "classpath:/jdbc.properties"})
 public class ApplicationConfig {
 
 
 //    @Autowired
 //    private Environment environment;
 
-//    @Bean
-//    public static PropertySourcesPlaceholderConfigurer pspc(){
-//        return new PropertySourcesPlaceholderConfigurer();
-//    }
+    // Needed for propertyLoader
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer pspc(){
+        return new PropertySourcesPlaceholderConfigurer();
+    }
 
     @Bean(initMethod = "init", destroyMethod = "destroy")
     public Greeting greeting(SpecialGuest specialGuest){
@@ -39,7 +43,6 @@ public class ApplicationConfig {
 
     @Bean @Scope("prototype")
     // public SpecialGuest specialGuest(@Value("${firstname}") String firstname, @Value("${lastname}") String lastname ) {
-    // environment.firstname.equals('Ivan')?'Foo':'Bar'}
     public SpecialGuest specialGuest(@Value("#{environment.firstname?:''}") String firstname, @Value("#{environment.lastname}") String lastname ) {
         return new SpecialGuest(firstname,lastname);
     }
@@ -47,6 +50,17 @@ public class ApplicationConfig {
     @Bean
     public Guest guest() {
         return new Guest("Reiner", "Unsinn");
+    }
+
+    @Bean
+    public DataSource dataSource(@Value("${jdbc.driverClassName}") String driverClassName, @Value("${jdbc.url}") String url, @Value("${jdbc.username}") String username, @Value("${jdbc.password}") String password) {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+
+        return dataSource;
     }
 
 
---
+++ lab/student/helloWorldSpring/src/test/java/net/gfu/seminar/spring/helloworld/DataSourceTest.java
@@ -0,0 +1,29 @@
+package net.gfu.seminar.spring.helloworld;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import static org.junit.Assert.assertTrue;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+/**
+ * Created by user1 on 12.01.2016.
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes={ApplicationConfig.class} )
+public class DataSourceTest {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Test
+    public void testValidConnection() throws SQLException{
+        assertTrue(dataSource.getConnection().isValid(1000));
+    }
+
+}
